da0ca4b2cb96ec632c08e1c30ec37ddcaf958ac9
Mike <Mhomolak@yahoo.com>
2016-10-06 11:38:48 -0600

    Initial commit

---
+++ public/javascripts/main.js
@@ -0,0 +1,43 @@
+(function(){
+
+  var app = angular.module('myApp',['ngRoute']);
+
+  // point to correct template for url...
+  app.config(['$routeProvider', function($routeProvider){
+
+    $routeProvider.when('/home',{
+      templateUrl:'templates/myAppHome.ejs',
+      controller:'myAppHomeCtrl'
+    }).otherwise( {redirectTo:'/' } )
+    
+  }])
+
+
+  // Service for backend...
+  app.service('backendService', ['$http', function($http){
+
+    // GET something...
+    $http.get()
+
+
+    // POST something...
+
+
+    // DELETE something...
+
+
+  }])
+
+
+  // list of controllers...
+  app.controller('myAppHomeCtrl', ['$scope', function($scope){
+
+    $scope.displayController = 'myAppHome';
+
+  }])
+
+
+
+
+
+})()
